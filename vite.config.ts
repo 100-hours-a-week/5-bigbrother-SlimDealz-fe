587e02b08ffd5e7e547beba52447039234d446c2
whdmldus1116 <76983867+whdmldus1116@users.noreply.github.com>
2024-08-28 14:07:58 +0900

    feat: dev와 맞춤

--- vite.config.ts
+++ vite.config.ts
@@ -45,7 +45,9 @@ export default defineConfig(({ command, mode }) => {
       port: 3000,
       proxy: {
         '/api': {
-          target: 'http://localhost:8080',
+          target: isProduction
+            ? 'http://api.slimdealz.store'
+            : 'http://localhost:8080',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, '') // 필요에 따라 경로를 재작성
         }
