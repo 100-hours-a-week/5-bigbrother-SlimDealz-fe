4dd9299d9c78995f28d0f1f898b23db3f2ada680
whdmldus1116 <76983867+whdmldus1116@users.noreply.github.com>
2024-08-28 13:38:54 +0900

    feat: sentry 오류수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  const isProduction = mode === 'production';
+  const isProduction = process.env.BUILD_APP_TYPE === 'production';
 
   return {
     resolve: {
