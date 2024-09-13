6bcbe48eb3658803db6c899e08462a93e7e37ef5
whdmldus1116 <76983867+whdmldus1116@users.noreply.github.com>
2024-09-13 11:09:22 +0900

    refactor: 최근본 상품 0 개 일때 조건추가

--- src/pages/user/recentlyView/index.tsx
+++ src/pages/user/recentlyView/index.tsx
@@ -1,21 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import { Container } from './styles';
 import PageNameTag from '@/components/tag/pageNameTag';
 import CategoryList from '@/components/list/categoryList';
+import { useNavigate } from 'react-router-dom';
+import { CustomBox, CustomButton } from '../bookmark/styles';
+import { Typography } from '@mui/material';
+import { Container } from '@/pages/main/styles';
 
 const UserRecentlyViewPage = () => {
   const [recentProducts, setRecentProducts] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const storedProducts = JSON.parse(
       localStorage.getItem('recentProducts') || '[]'
     );
-    setRecentProducts(storedProducts);
+    const filteredProducts = storedProducts.filter(
+      (product: any) => product.prices?.[0]?.setPrice !== undefined
+    );
+    setRecentProducts(filteredProducts);
   }, []);
 
   return (
     <Container>
-      <PageNameTag pageName="최근 본 상품" />
+      {recentProducts.length > 0 && (
+        <PageNameTag pageName={`전체 ${recentProducts.length}개`} />
+      )}
       {recentProducts.length > 0 ? (
         recentProducts.map((product: any) => (
           <CategoryList
@@ -24,11 +33,23 @@ const UserRecentlyViewPage = () => {
             image={product.imageUrl}
             name={product.name}
             shipping={product.shippingFee}
-            price={product.prices?.[0]?.setPrice || '가격 없음'}
+            price={product.prices?.[0]?.setPrice}
           />
         ))
       ) : (
-        <div>최근 본 상품이 없습니다.</div>
+        <>
+          <CustomBox>
+            <Typography variant="h6" gutterBottom>
+              최근 본 상품이 없습니다
+            </Typography>
+            <Typography variant="body2" color="textSecondary">
+              지금 바로 상품을 클릭 해보세요.
+            </Typography>
+            <CustomButton onClick={() => navigate('/')}>
+              상품 보러가기
+            </CustomButton>
+          </CustomBox>
+        </>
       )}
     </Container>
   );
